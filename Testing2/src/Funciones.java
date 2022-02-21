50408d00e479b2bda44ad56e22fbb3e1a1d21bdb
David <David@DESKTOP-1VREN8U>
2022-02-21 12:32:59 +0100

    Prueba de testing, javadoc y jar

---
+++ Testing2/src/Funciones.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+
+
+public class Funciones {
+	
+	
+	public String diaSemana(int dia) {
+		String res="Domingo";
+		// Switch con int como condicion
+		switch (dia) {
+		    case 1:
+		       res="Domingo";
+		        break;
+		    case 2:
+		    	res="Lunes";
+		        break;
+		    case 3:
+		    	res="Martes";
+		        break;
+		    case 4:
+		    	res="Miercoles";
+		        break;
+		    case 5:
+		    	res="Jueves";
+		        break;
+		    case 6:
+		    	res="Viernes";
+		        break;
+		    case 7:
+		    	res="Sabado";
+		        break;
+
+		}
+		return res;
+	}
+
+	int getDiasMes(String mes, boolean bisiesto) {
+		int dias = 30;
+		
+		if (mes.equalsIgnoreCase("enero") || mes.equalsIgnoreCase("marzo") || mes.equalsIgnoreCase("mayo") || 
+				mes.equalsIgnoreCase("julio") ||mes.equalsIgnoreCase("agosto") || mes.equalsIgnoreCase("octubre") ||
+				mes.equalsIgnoreCase("diciembre") ) {
+			dias=30;
+		} else if (mes.equalsIgnoreCase("febrero")){
+			if (bisiesto) {
+				dias=29;
+			} else {
+				dias=29;
+			}
+		}
+		return dias;
+	}
+	
+	public static boolean esTriangularSup(int matriz [][]) {
+		boolean triangsup=true;
+		
+		for (int i = 1; i < matriz.length && triangsup; i++) {
+			for (int j = 0; j <i && triangsup; j++) {
+				if (matriz[i][j]!=0) {
+					triangsup=false;
+				} 
+			}
+		}
+		return triangsup;
+	}
+
+	
+}
